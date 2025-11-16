b58237f89cf755e6e751b5243cfc42951c2f29c0
VougneFroid <142602426+VougneFroid@users.noreply.github.com>
2025-11-16 20:46:51 +0800

    removed bloat

--- consultation_scheduler/consultations/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsProfessor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.user_type == 'professor'
-
-class IsStudent(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.user_type == 'student'
